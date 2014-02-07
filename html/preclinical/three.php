67ed83d0332e4a68706de877ef0801bda3eca678
ornette coleman <omniosi@gmail.com>
2014-02-06 22:05:20 -0500

    preclinical page three done

--- html/preclinical/three.php
+++ html/preclinical/three.php
@@ -1,8 +1,22 @@
 <?php require_once( '../../couch/cms.php' ); ?>
 <?php $current_page = 'p-three' ?>
 <cms:template title='GSS Pre-Clinical Three' commentable='1'>
+	<cms:editable name='group_column1' label='First Column' desc='First column of logos' type='group' />
+	<cms:editable name='logo_one' label='Logo One' desc='First image in column one' width='135' type='image' />
+	<cms:editable name='logo_two' label='Logo Two' desc='Second image in column one' width='135' type='image' />
+	<cms:editable name='logo_three' label='Logo Three' desc='Third image in column one' width='135' type='image' />
+	<cms:editable name='logo_four' label='Logo Four' desc='Fourth image in column one' width='135' type='image' />
+	<cms:editable name='logo_five' label='Logo Five' desc='Fifth image in column one' width='135' type='image' />
+	<cms:editable name='logo_six' label='Logo Six' desc='Sixth image in column one' width='135' type='image' />
 
-	<cms:editable name='content_image' label='Image' desc='Accompanying image for main text' width='130' type='image' />
+	<cms:editable name='group_column2' label='Second Column' desc='Second column of logos' type='group' />
+	<cms:editable name='logoB_one' label='Logo One' desc='First image in column two' width='135' type='image' />
+	<cms:editable name='logoB_two' label='Logo Two' desc='Second image in column two' width='135' type='image' />
+	<cms:editable name='logoB_three' label='Logo Three' desc='Third image in column two' width='135' type='image' />
+	<cms:editable name='logoB_four' label='Logo Four' desc='Fourth image in column two' width='135' type='image' />
+	<cms:editable name='logoB_five' label='Logo Five' desc='Fifth image in column two' width='135' type='image' />
+	<cms:editable name='logoB_six' label='Logo Six' desc='Sixth image in column two' width='135' type='image' />
+
 	<cms:editable name='content_main' label='Main Text' desc='Main Text Area' type='richtext' />
 	<cms:editable name='content_sidebar_column_1' label='Sidebar Column One' desc='Sidebar Text Column One' type='richtext' />
 
@@ -22,34 +36,34 @@
 		<div class="container">
 			<div class="main">
 
-			<span class="contentnav left col">
- 				<a href="four.php" data-transition="slide" data-direction="reverse">
-					<i class="icon-chevron-left icon-4x"></i>
-				</a>
-			</span>
+				<span class="contentnav left col">
+	 				<a href="four.php" data-transition="slide" data-direction="reverse">
+						<i class="icon-chevron-left icon-4x"></i>
+					</a>
+				</span>
 
 				<div class="col col-main">
 					<ul class="logobox">
 						<li class="col-two">
 							<ul>
-								<li class=""><img src="../../img/logobox-amgen.jpg" width="135" height="70"></li>
-								<li class=""><img src="../../img/logobox-lantheus.jpg" width="135" height="70"></li>
-								<li class=""><img src="../../img/logobox-momenta.jpg" width="135" height="70"></li>
-								<li class=""><img src="../../img/logobox-mmrf.jpg" width="135" height="70"></li>
-								<li class=""><img src="../../img/logobox-purdue.jpg" width="135" height="70"></li>
-								<li class=""><img src="../../img/logobox-shire.jpg" width="135" height="70"></li>
+								<li><img src="<cms:show logo_one />" width="135"></li>
+								<li><img src="<cms:show logo_two />" width="135"></li>
+								<li><img src="<cms:show logo_three />" width="135"></li>
+								<li><img src="<cms:show logo_four />" width="135"></li>
+								<li><img src="<cms:show logo_five />" width="135"></li>
+								<li><img src="<cms:show logo_six />" width="135"></li>
 							</ul>
 						</li>
 					</ul>
 					<ul class="logobox">
 						<li class="col-two">
 							<ul>
-								<li class=""><img src="../../img/logobox-flexion.jpg" width="135" height="70"></li>
-								<li class=""><img src="../../img/logobox-merck.jpg" width="135" height="70"></li>
-								<li class=""><img src="../../img/logobox-mylan.jpg" width="135" height="70"></li>
-								<li class=""><img src="../../img/logobox-pac-bio.jpg" width="135" height="70"></li>
-								<li class=""><img src="../../img/logobox-roche.jpg" width="135" height="70"></li>
-								<li class=""><img src="../../img/logobox-aveo.jpg" width="135" height="70"></li>
+								<li><img src="<cms:show logoB_one />" width="135"></li>
+								<li><img src="<cms:show logoB_two />" width="135"></li>
+								<li><img src="<cms:show logoB_three />" width="135"></li>
+								<li><img src="<cms:show logoB_four />" width="135"></li>
+								<li><img src="<cms:show logoB_five />" width="135"></li>
+								<li><img src="<cms:show logoB_six />" width="135"></li>
 							</ul>
 						</li>
 					</ul>
